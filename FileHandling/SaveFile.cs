bcdaf98656256f3cb0ec255449cbf77519449ce3
TinyMan256 <54464793+TinyMan256@users.noreply.github.com>
2020-08-07 13:06:33 +0200

    Add files via upload

---
+++ FileHandling/SaveFile.cs
@@ -0,0 +1,33 @@
+﻿
+using System.IO;
+
+namespace Tiny_Injector
+{
+    public static class SaveFile
+    {
+        public static string name = "Tiny Man Injector Save Data.txt";
+
+        public static void saveFile()
+        {
+            if (!File.Exists(name))
+            {
+                File.Create(name).Close();
+            }                       
+
+                File.WriteAllText(name, DataHolder.getData());
+        }
+
+        public static void loadFile()
+        {
+            if (!File.Exists(name))
+            {
+                File.Create(name).Close();
+                return;
+            }
+
+            if (DataHolder.setData(File.ReadAllLines(name)) == 0)
+                Imports.kernelErrMsg("Save file has no valid content");
+        }
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("Tiny Man Injector")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tiny Man Injector")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: ComVisible(false)]
+
+
+[assembly: Guid("5b068901-8792-405d-8a19-ef99aad85a84")]
+
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
